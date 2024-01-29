1c3700851baeac456850a39538fe8c43879858b8
Zhminko Roman <zhminkors@gmail.com>
2024-01-30 00:13:23 +0100

    feat: complete lesson 30

---
+++ lessons/30/src/shaders/fragment.glsl
@@ -0,0 +1,14 @@
+varying vec3 vColor;
+
+void main() {
+  float strength = distance(gl_PointCoord, vec2(0.5));
+  // strength *= 2.0;
+  // strength = step(0.5, strength);
+  strength = 1.0 - strength;
+  strength = pow(strength, 10.0);
+
+  // vec3 color = mix(vec3(0.0), vColor, strength);
+  gl_FragColor = vec4(vColor, strength);
+    #include <colorspace_fragment>
+
+}
